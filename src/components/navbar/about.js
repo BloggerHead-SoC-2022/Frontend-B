7ecf2156fad43a62527b1e7e2b9ca8eadf1f45c5
AkshGarg19 <akshgarg19112002@gmail.com>
2022-07-19 18:35:36 +0530

    Committed

--- src/components/navbar/about.js
+++ src/components/navbar/about.js
@@ -1,11 +1,32 @@
 import React from "react";
 
+
 export const About =()=>{
     return (
-        <div >
-            <h1 className="bg-info text-dark p-5 mt-5 mx-auto">
-                this is the about page.
-            </h1>
+        <div className="shadow  p-3 my-3 mx-3" >
+            <div className=" p-3">
+                <h1 className="text-center">SoC Project</h1>
+                <h2 className="text-center text-info">Intro to MERN - BloggerHead</h2>
+                <h3 className="text-center text-muted">Group-B</h3>
+                <div className=" my-3">
+
+                Mentees :
+                        <ul>
+                        <li>Aksh Garg</li>
+                        <li>Arpana Prajapati</li>
+                        <li>Love Jaiswal</li>
+                        <li>Shriyank Tatawat</li>
+                        </ul>
+
+				Mentors :
+                       <ul>
+                        <li>Dhananjay Kejriwal</li>
+                        <li>Sartaj Islam</li>
+                        <li>Mihika Dhok</li>
+                        </ul>
+						
+                </div>
+            </div>
         </div>
     )
 }
