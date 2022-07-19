562107862ea2b43e8efe81b5c01ec3228393c99d
AkshGarg19 <akshgarg19112002@gmail.com>
2022-07-19 16:48:18 +0530

    newEdit

---
+++ src/components/navbar/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About =()=>{
+    return (
+        <div >
+            <h1 className="bg-info text-dark p-5 mt-5 mx-auto">
+                this is the about page.
+            </h1>
+        </div>
+    )
+}
